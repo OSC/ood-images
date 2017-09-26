5e5821ed7e716c583cd532eb5edf70ba3bf272e3
Trey Dockendorf <tdockendorf@osc.edu>
2017-09-26 08:45:02 -0400

    Update ood-setup.sh to match documentation for OOD 1.0

--- ood-setup.sh
+++ ood-setup.sh
@@ -16,7 +16,8 @@ mkdir -p /opt/ood/src
 cd /opt/ood/src
 scl enable git19 -- git clone https://github.com/OSC/ood-portal-generator.git
 cd ood-portal-generator
-scl enable git19 -- git reset --hard v0.3.1
+scl enable git19 -- git reset --hard v0.4.0
+scl enable rh-ruby22 -- rake
 scl enable rh-ruby22 -- rake install
 
 # Add user to system and apache basic auth
@@ -29,7 +30,7 @@ scl enable httpd24 -- htpasswd -b -c /opt/rh/httpd24/root/etc/httpd/.htpasswd oo
 cd /opt/ood/src
 scl enable git19 -- git clone https://github.com/OSC/mod_ood_proxy.git
 cd mod_ood_proxy
-scl enable git19 -- git reset --hard v0.2.0
+scl enable git19 -- git reset --hard v0.3.1
 scl enable rh-ruby22 -- rake install
 
 # Install nginx_stage
@@ -40,8 +41,8 @@ scl enable git19 -- git reset --hard v0.2.1
 scl enable rh-ruby22 -- rake install
 
 # Configure nginx_stage
-sed -i 's/^#exec scl/exec scl/g' /opt/ood/nginx_stage/bin/ood_ruby
-cat > /etc/sudoers.d/nginx_stage <<EOF
+install -o root -g root -m 0440 /dev/null /etc/sudoers.d/ood
+cat > /etc/sudoers.d/ood <<EOF
 Defaults:apache !requiretty,!authenticate
 apache ALL=(ALL) NOPASSWD: /opt/ood/nginx_stage/sbin/nginx_stage
 EOF
@@ -56,52 +57,9 @@ scl enable rh-ruby22 -- rake install
 # App installations
 mkdir -p /etc/ood/config/clusters.d
 mkdir -p /var/www/ood/apps/sys
-# dashboard
-cd /var/www/ood/apps/sys
-scl enable git19 -- git clone https://github.com/OSC/ood-dashboard.git dashboard
-cd dashboard
-scl enable git19 -- git reset --hard v1.10.0
-scl enable rh-ruby22 -- bin/bundle install --path=vendor/bundle
-scl enable rh-ruby22 nodejs010 -- bin/rake assets:precompile RAILS_ENV=production
-scl enable rh-ruby22 nodejs010 -- bin/rake tmp:clear
-
-# shell
-cd /var/www/ood/apps/sys
-scl enable git19 -- git clone https://github.com/OSC/ood-shell.git shell
-cd shell
-scl enable git19 -- git reset --hard v1.1.2
-scl enable git19 nodejs010 -- npm install
-
-# files
-cd /var/www/ood/apps/sys
-scl enable git19 -- git clone https://github.com/OSC/ood-fileexplorer.git files
-cd files
-scl enable git19 -- git reset --hard v1.3.1
-scl enable git19 nodejs010 -- npm install
-
-# file-editor
-cd /var/www/ood/apps/sys
-scl enable git19 -- git clone https://github.com/OSC/ood-fileeditor file-editor
-cd file-editor
-scl enable git19 -- git reset --hard v1.2.3
-scl enable rh-ruby22 -- bin/bundle install --path=vendor/bundle
-scl enable rh-ruby22 nodejs010 -- bin/rake assets:precompile RAILS_ENV=production
-scl enable rh-ruby22 nodejs010 -- bin/rake tmp:clear
-
-# activejobs
-cd /var/www/ood/apps/sys
-scl enable git19 -- git clone https://github.com/OSC/ood-activejobs activejobs
-cd activejobs
-scl enable git19 -- git reset --hard v1.3.1
-scl enable rh-ruby22 -- bin/bundle install --path=vendor/bundle
-scl enable rh-ruby22 nodejs010 -- bin/rake assets:precompile RAILS_ENV=production
-scl enable rh-ruby22 nodejs010 -- bin/rake tmp:clear
-
-# myjobs
-cd /var/www/ood/apps/sys
-scl enable git19 -- git clone https://github.com/OSC/ood-myjobs myjobs
-cd myjobs
-scl enable git19 -- git reset --hard v2.1.2
-scl enable rh-ruby22 git19 -- bin/bundle install --path=vendor/bundle
-scl enable rh-ruby22 nodejs010 -- bin/rake assets:precompile RAILS_ENV=production
-scl enable rh-ruby22 nodejs010 -- bin/rake tmp:clear
+cd /opt/ood
+scl enable git19 -- git clone https://github.com/OSC/ood-apps-installer.git apps
+cd apps
+scl enable git19 -- git reset --hard v1.0.0
+scl enable rh-ruby22 nodejs010 git19 -- rake
+scl enable rh-ruby22 -- rake install
