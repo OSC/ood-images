f11555934b542540da1d78df33f97780aa7a8bdf
Jeff Ohrstrom <johrstrom@hotmail.com>
2019-10-02 16:12:28 -0400

    remove podman stack
    I'm going to remove the podman stack becuase really we don't need it.
    As long as users are able to map the ood user (uid:1000 gid:1000) to
    themselves, they will be able to map their files to ood inside the
    container. They can also override the launch-httpd to do any other
    init config they need.

--- common/ood-setup.sh
+++ common/ood-setup.sh
@@ -11,7 +11,9 @@ fi
 # Add user to system and apache basic auth
 groupadd ood
 useradd --create-home --gid ood ood
-echo -n "ood" | passwd --stdin ood
+passwd --delete root
+passwd --delete ood
+usermod -a -G wheel ood
 scl enable httpd24 -- htpasswd -b -c /opt/rh/httpd24/root/etc/httpd/.htpasswd ood ood
 
 # Misc
